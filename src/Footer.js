51a21438384ecd5f3930709be74d51391e8c1060
ahmedfadl156 <af38765222@gmail.com>
2025-08-06 22:25:54 +0300

    added-footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Developed by Ahmed Fadl</p>
+      <div className="social-media">
+        <a href="https://github.com/ahmed-fadl" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://linkedin.com/in/ahmed-fadl" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://twitter.com/ahmed_fadl" target="_blank" rel="noopener noreferrer">Twitter</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
